94a20acdb4dca780607cc68f2dbd89ddaed42254
Serban Stancu <stancu@adobe.com>
2016-05-12 13:44:39 -0600

    Move webpack config in a separate file.

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ var argv = require('yargs')
   .alias('source-maps', 'with-source-maps')
   .argv;
 
+var webpackConfig = require('./webpack.config');
+
 // Shows an growl notification saying that building failed and then logs the error to the console.
 var errorAlert = function(error) {
   notify.onError({
@@ -22,52 +24,6 @@ var errorAlert = function(error) {
   console.log(error.toString());
 
   this.emit('end');
-};
-
-var webpackConfig = {
-  output: {
-    filename: 'view.js'
-  },
-  devtool: argv.sourceMaps ? '#inline-source-map' : false,
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        include: /src\/view/,
-        exclude: /__tests__/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        }
-      },
-      {
-        test: /\.pattern$/,
-        include: /src\/view/,
-        loader: 'style-loader!css-loader?sourceMap!stylus-loader!import-glob-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx', 'styl'],
-    // Needed when npm-linking projects like coralui-support-react
-    // https://github.com/webpack/webpack/issues/784
-    fallback: path.join(__dirname, 'node_modules'),
-    // When looking for modules, prefer the node_modules within this project. An example where
-    // this is helpful: If this project requires in module X, we've npm linked module X and
-    // module X has its node_modules populated, and module X requires React, typically two
-    // copies of React would be bundled (one from this project's node_modules and one from
-    // module X's node_modules). By setting this root, module X will prefer the React in this
-    // project's node_modules, effectively de-duping React.
-    // https://github.com/webpack/webpack/issues/966
-    root: path.resolve(__dirname, 'node_modules')
-  },
-  stylus: {
-    use: [require('nib')()],
-    import: [
-      path.resolve('./node_modules/nib/lib/nib/index'),
-      path.resolve('./src/view/sharedVariables')
-    ]
-  }
 };
 
 gulp.task('buildJS', function() {
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var path = require('path');
+var argv = require('yargs')
+  .default('watch', true)
+  .alias('source-maps', 'with-source-maps')
+  .argv;
+
+module.exports = {
+  output: {
+    filename: 'view.js'
+  },
+  devtool: argv.sourceMaps ? '#inline-source-map' : false,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: /src\/view/,
+        exclude: /__tests__/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        }
+      },
+      {
+        test: /\.pattern$/,
+        include: /src\/view/,
+        loader: 'style-loader!css-loader?sourceMap!stylus-loader!import-glob-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', 'styl'],
+    // Needed when npm-linking projects like coralui-support-react
+    // https://github.com/webpack/webpack/issues/784
+    fallback: path.join(__dirname, 'node_modules'),
+    // When looking for modules, prefer the node_modules within this project. An example where
+    // this is helpful: If this project requires in module X, we've npm linked module X and
+    // module X has its node_modules populated, and module X requires React, typically two
+    // copies of React would be bundled (one from this project's node_modules and one from
+    // module X's node_modules). By setting this root, module X will prefer the React in this
+    // project's node_modules, effectively de-duping React.
+    // https://github.com/webpack/webpack/issues/966
+    root: path.resolve(__dirname, 'node_modules')
+  },
+  stylus: {
+    use: [require('nib')()],
+    import: [
+      path.resolve('./node_modules/nib/lib/nib/index'),
+      path.resolve('./src/view/sharedVariables')
+    ]
+  }
+};
