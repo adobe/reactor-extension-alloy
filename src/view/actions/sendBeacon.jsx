886af177de6bf7aa72cf9a2f480ed8269f1cad5f
Aaron Hardy <aahardy@adobe.com>
2016-05-13 16:46:43 -0600

    Implemented send beacon action.

--- src/view/actions/sendBeacon.jsx
+++ src/view/actions/sendBeacon.jsx
@@ -1,20 +1,144 @@
 import React from 'react';
+import Coral from '@coralui/coralui-support-reduxform';
+import { DataElementSelectorButton } from '@reactor/react-components';
+import openDataElementSelector from '../utils/openDataElementSelector';
 import extensionViewReduxForm from '../extensionViewReduxForm';
+import mergeFormConfigs from '../utils/mergeFormConfigs';
+import ConfigurationSelector, { formConfig as configurationSelectorFormConfig } from './components/configurationSelector';
+
+const TYPES = {
+  PAGE: 'page',
+  LINK: 'link'
+};
+
+const LINK_TYPES = {
+  CUSTOM: 'o',
+  DOWNLOAD: 'd',
+  EXIT: 'e'
+};
 
 class SendBeacon extends React.Component {
   render() {
-    return <div>Send Beacon</div>;
+    const {
+      type,
+      linkType,
+      linkName
+    } = this.props.fields;
+
+    let linkNameLabel;
+
+    switch (linkType.value) {
+      case LINK_TYPES.DOWNLOAD:
+        linkNameLabel = 'File Name';
+        break;
+      case LINK_TYPES.EXIT:
+        linkNameLabel = 'Destination URL';
+        break;
+      default:
+        linkNameLabel = 'Link Name';
+    }
+
+    return (
+      <div>
+        <ConfigurationSelector className="u-gapBottom" fields={this.props.fields}/>
+        <h4 className="coral-Heading coral-Heading--4">Tracking</h4>
+        <div>
+          <Coral.Radio
+            {...type}
+            value="page"
+            checked={type.value === TYPES.PAGE}>
+            Increment a pageview<span className="SendBeacon-trackerApi"> - s.t()</span>
+          </Coral.Radio>
+        </div>
+        <div>
+          <Coral.Radio
+            {...type}
+            value="link"
+            checked={type.value === TYPES.LINK}>
+            Do not increment a pageview<span className="SendBeacon-trackerApi"> - s.tl()</span>
+          </Coral.Radio>
+          {
+            type.value === TYPES.LINK ?
+              <div className="FieldSubset SendBeacon-linkDetails">
+                <div className="SendBeacon-linkType u-gapRight">
+                  <label>
+                    <span className="Label">Link Type</span>
+                    <div>
+                      <Coral.Select className="Field--short" {...linkType}>
+                        <Coral.Select.Item value={LINK_TYPES.CUSTOM}>
+                          Custom Link
+                        </Coral.Select.Item>
+                        <Coral.Select.Item value={LINK_TYPES.DOWNLOAD}>
+                          Download Link
+                        </Coral.Select.Item>
+                        <Coral.Select.Item value={LINK_TYPES.EXIT}>
+                          Exit Link
+                        </Coral.Select.Item>
+                      </Coral.Select>
+                    </div>
+                  </label>
+                </div>
+                <div className="SendBeacon-linkName">
+                  <label>
+                    <span className="Label">{linkNameLabel}</span>
+                    <div>
+                      <Coral.Textfield className="Field--long" {...linkName}/>
+                      <DataElementSelectorButton
+                        onClick={openDataElementSelector.bind(this, linkName)}/>
+                    </div>
+                  </label>
+                </div>
+              </div> : null
+            }
+        </div>
+      </div>
+    );
   }
 }
 
-const formConfig = {
-  fields: [],
-  formValuesToSettings(settings, values) {
-    return {
-      //browsers: values.browsers || [] // An array is required.
-    };
-  }
-};
+const formConfig = mergeFormConfigs(
+  configurationSelectorFormConfig, {
+    fields: [
+      'type',
+      'linkType',
+      'linkName'
+    ],
+    settingsToFormValues(values, options) {
+      const {
+        type,
+        linkType,
+        linkName
+      } = options.settings || {};
+
+      return {
+        ...values,
+        type: type || TYPES.PAGE,
+        linkType: linkType || LINK_TYPES.CUSTOM,
+        linkName
+      };
+    },
+    formValuesToSettings(settings, values) {
+      const {
+        type,
+        linkType,
+        linkName
+      } = values;
+
+      settings = {
+        ...settings,
+        type
+      };
+
+      if (type === TYPES.LINK) {
+        settings.linkType = linkType;
+        if (linkName) {
+          settings.linkName = linkName;
+        }
+      }
+
+      return settings;
+    }
+  });
 
 export default extensionViewReduxForm(formConfig)(SendBeacon);
 
