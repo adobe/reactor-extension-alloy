e20adfbb24a751c40c030643cd226b6576f12d33
Aaron Hardy <aahardy@adobe.com>
2016-10-04 15:49:04 -0600

    Refactoring ReactCoral component adaptation.

--- src/view/components/coralField.jsx
+++ src/view/components/coralField.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import { ValidationWrapper, DataElementSelectorButton } from '@reactor/react-components';
-import { Field as ReduxFormField } from 'redux-form';
-import adaptReactCoral from '../adaptReactCoral';
+import { Field } from 'redux-form';
+import ReduxFormAutocompleteSelect from '../reduxFormAutocompleteSelect';
 import addDataElementToken from '../utils/addDataElementToken';
+
+const eventHandlerRegex = /^on[A-Z]/;
+
+/**
+ * If a consumer specifies an event handler (say, onChange), we need to merge it with any handler
+ * that redux-form provides for the same event type otherwise it will get overridden by redux-forms
+ * handler. We do this by providing a wrapper handler which calls redux-form's handler first,
+ * then the consumer-defined handler.
+ * @param reduxFormProvidedProps
+ * @param consumerProvidedProps
+ */
+const mergeHandlers = (reduxFormProvidedProps, consumerProvidedProps) => {
+  Object.keys(consumerProvidedProps).forEach(consumerProvidedPropName => {
+    if (reduxFormProvidedProps[consumerProvidedPropName] &&
+      eventHandlerRegex.test(consumerProvidedPropName)) {
+      const reduxFormHandler = reduxFormProvidedProps[consumerProvidedPropName];
+      const consumerHandler = consumerProvidedProps[consumerProvidedPropName];
+
+      reduxFormProvidedProps[consumerProvidedPropName] = (...args) => {
+        reduxFormHandler(...args);
+        consumerHandler(...args);
+      };
+
+      delete consumerProvidedProps[consumerProvidedPropName];
+    }
+  });
+};
 
 class InputWrapper extends React.Component {
   openDataElementSelector = () => {
@@ -22,27 +49,46 @@ class InputWrapper extends React.Component {
     const {
       input,
       meta,
-      reactCoralComponent,
+      reactCoralComponent: ReactCoralComponent,
       supportValidation,
       supportDataElement,
       className,
-      componentClassName,
       suffixLabel,
-      ...rest
+      componentClassName,
+      ...otherProps
     } = this.props;
 
-    const AdaptedReactCoralComponent = adaptReactCoral(reactCoralComponent);
+    mergeHandlers(input, otherProps);
 
     // I bet there's a way to cut down on the number of span wrappers.
 
+    let inputComponent;
+
+    // We match the name instead of Component === Autocomplete because that would require us to
+    // require in the React-Coral class (and therefore it would end up in the app build)
+    // even if we never ended up using the class.
+    if (ReactCoralComponent.name === 'Select' || ReactCoralComponent.name === 'Autocomplete') {
+      inputComponent = (
+        <ReduxFormAutocompleteSelect
+          component={ ReactCoralComponent }
+          className={ componentClassName }
+          { ...otherProps }
+          { ...input }
+        />
+      );
+    } else {
+      inputComponent = (
+        <ReactCoralComponent
+          className={ componentClassName }
+          { ...otherProps }
+          { ...input }
+        />
+      );
+    }
+
     let result = (
       <span>
-        <AdaptedReactCoralComponent
-          className={ componentClassName }
-          input={ input }
-          meta={ meta }
-          { ...rest }
-        />
+        { inputComponent }
         {
           suffixLabel ? <span className="Label u-gapLeft">{ suffixLabel }</span> : null
         }
@@ -78,7 +124,7 @@ class InputWrapper extends React.Component {
   }
 }
 
-const Field = ({ component, ...otherProps }) => {
+const CoralField = ({ component, ...otherProps }) => {
   // Redux-form will only provide a "checked" property if you specify type="radio" or
   // type="checkbox". We'll do that automatically here so we don't have to remember to do it
   // every time we use radio or checkbox.
@@ -88,7 +134,7 @@ const Field = ({ component, ...otherProps }) => {
   }
 
   return (
-    <ReduxFormField
+    <Field
       reactCoralComponent={ component }
       component={ InputWrapper }
       { ...otherProps }
@@ -96,4 +142,4 @@ const Field = ({ component, ...otherProps }) => {
   );
 };
 
-export default Field;
+export default CoralField;
