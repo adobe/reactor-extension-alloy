8d0da84c65002f8f2a03db3a36c02066ee4f2f7d
Aaron Hardy <aahardy@adobe.com>
2017-01-30 14:16:20 -0700

    Added copyright banner.

--- src/lib/helpers/generateVersion.js
+++ src/lib/helpers/generateVersion.js
@@ -1,3 +1,21 @@
+/*************************************************************************
+* ADOBE CONFIDENTIAL
+* ___________________
+*
+*  Copyright 2016 Adobe Systems Incorporated
+*  All Rights Reserved.
+*
+* NOTICE:  All information contained herein is, and remains
+* the property of Adobe Systems Incorporated and its suppliers,
+* if any.  The intellectual and technical concepts contained
+* herein are proprietary to Adobe Systems Incorporated and its
+* suppliers and are protected by all applicable intellectual property
+* laws, including trade secret and copyright laws.
+* Dissemination of this information or reproduction of this material
+* is strictly forbidden unless prior written permission is obtained
+* from Adobe Systems Incorporated.
+**************************************************************************/
+
 // The DTM mod-stats code version is a 4 characters string.  The first character will always be D
 // followed by year, month, and day codes.
 // For example: JS-1.4.3-D53O = JS 1.4.3 code, DTM 2015 March 24th release (revision 1)
