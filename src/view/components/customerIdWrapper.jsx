83f5a9408b9d8892e5f73a028f5ba739ceb66998
Willi Gamboa <wgamboa@adobe.com>
2019-12-05 14:47:47 -0500

    CORE-33719-2 addressed pull request comments 2

--- src/view/components/customerIdWrapper.jsx
+++ src/view/components/customerIdWrapper.jsx
@@ -1,5 +1,5 @@
 import "regenerator-runtime"; // needed for some of react-spectrum
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { FieldArray } from "formik";
 import Textfield from "@react/react-spectrum/Textfield";
 import Checkbox from "@react/react-spectrum/Checkbox";
@@ -16,181 +16,161 @@ import PropTypes from "prop-types";
 import WrappedField from "./wrappedField";
 import authenticatedStateOptions from "../constants/authenticatedStateOptions";
 import getDefaultCustomerId from "../utils/getDefaultCustomerId";
-import ErrorMessage from "./errorMessage";
 import InfoTipLayout from "./infoTipLayout";
 import getInstanceOptions from "../utils/getInstanceOptions";
 
 function CustomerIdWrapper({ values, initInfo }) {
-  // const [namespaceOptions, setNamespaceOptions] = useState([]);
-  const [showStuff, setShowStuff] = useState({});
-  let returnValue = null;
+  // TODO: implement in the future
+  /*
+  const [namespaceOptions, setNamespaceOptions] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchData() {
       try {
-        // const nOptions = await loadIdentityNamespaces();
+        const nOptions = await loadIdentityNamespaces();
 
-        setShowStuff({
-          value: "content"
-        });
-        // setNamespaceOptions(nOptions);
-      } catch (error) {
-        setShowStuff({
-          value: "error",
-          message: error.message
-        });
-      }
+        setNamespaceOptions(nOptions);
+      } catch (error) {}
     }
 
     fetchData();
   }, []);
+*/
 
-  switch (showStuff.value) {
-    case "content":
-      returnValue = (
-        <React.Fragment>
-          <FieldLabel labelFor="instanceName" label="Instance" />
-          <div>
-            <WrappedField
-              id="instanceName"
-              name="instanceName"
-              component={Select}
-              componentClassName="u-fieldLong"
-              options={getInstanceOptions(initInfo)}
-            />
-          </div>
-          <FieldArray
-            name="customerIds"
-            render={arrayHelpers => {
-              return (
-                <React.Fragment>
-                  <div className="u-gapTop u-alignRight">
-                    <Button
-                      id="addCustomerId"
-                      label="Add Customer ID"
-                      onClick={() => {
-                        arrayHelpers.push(getDefaultCustomerId());
-                      }}
-                    />
-                  </div>
-                  {((Array.isArray(values.customerIds) &&
-                    values.customerIds.length) ||
-                    null) && (
-                    <Heading variant="subtitle2">Customer IDs</Heading>
-                  )}
-                  <div>
-                    {((Array.isArray(values.customerIds) &&
-                      values.customerIds.length) ||
-                      null) &&
-                      values.customerIds.map((customerId, index) => (
-                        <Well key={index}>
-                          <div>
-                            <FieldLabel
-                              labelFor="namespaceField"
-                              label="Namespace"
-                            />
-                            <div>
-                              <WrappedField
-                                id="namespaceField"
-                                name={`customerIds.${index}.namespace`}
-                                // component={Select}
-                                component={Textfield}
-                                componentClassName="u-fieldLong"
-                                // options={namespaceOptions}
-                              />
-                            </div>
-                          </div>
-                          <div className="u-gapTop">
-                            <FieldLabel labelFor="idField" label="ID" />
-                            <div>
-                              <WrappedField
-                                id="idField"
-                                name={`customerIds.${index}.id`}
-                                component={Textfield}
-                                componentClassName="u-fieldLong"
-                                supportDataElement
-                              />
-                            </div>
-                          </div>
-                          <div className="u-gapTop">
-                            <InfoTipLayout tip="Uses the SHA-256 hashing algorithm that allows you to pass in customer IDs or email addresses, and pass out hashed IDs. This is an optional Javascript method for sending hashed identifiers. You can continue to use your own methods of hashing prior to sending customer IDs.">
-                              <WrappedField
-                                name={`customerIds.${index}.hashEnabled`}
-                                component={Checkbox}
-                                label="Convert ID to sha256 hash"
-                              />
-                            </InfoTipLayout>
-                          </div>
-                          <div className="u-gapTop">
-                            <FieldLabel
-                              labelFor="authenticatedStateField"
-                              label="Authenticated State"
-                            />
-                            <div>
-                              <WrappedField
-                                id="authenticatedStateField"
-                                name={`customerIds.${index}.authenticatedState`}
-                                component={Select}
-                                componentClassName="u-fieldLong"
-                                options={authenticatedStateOptions}
-                              />
-                            </div>
-                          </div>
-                          <div className="u-gapTop">
-                            <InfoTipLayout tip="Adobe Experience Platform will use the customer ID as an identifier to help stitch together more information about that individual. If left unchecked, the identifier within this namespace will still be collected but the ECID will be used as the primary identifier for stitching.">
-                              <WrappedField
-                                name={`customerIds.${index}.primary`}
-                                component={Checkbox}
-                                label="Primary"
-                              />
-                            </InfoTipLayout>
-                          </div>
-                          <div className="u-gapTop">
-                            <ModalTrigger>
-                              <Button
-                                id={`deleteButton${index}`}
-                                label="Delete Customer ID"
-                                icon={<Delete />}
-                                variant="action"
-                                disabled={values.customerIds.length === 1}
-                              />
-                              {values.customerIds.length === 1 ? (
-                                <span className="Note u-gapLeft">
-                                  You must have at least one customer ID to use
-                                  this action.
-                                </span>
-                              ) : null}
-                              <Dialog
-                                title="Delete Customer ID"
-                                id={`deleteCustomerId${index}`}
-                                onConfirm={() => {
-                                  arrayHelpers.remove(index);
-                                }}
-                                confirmLabel="Delete"
-                                cancelLabel="Cancel"
-                              >
-                                Would you like to proceed?
-                              </Dialog>
-                            </ModalTrigger>
-                          </div>
-                        </Well>
-                      ))}
-                  </div>
-                </React.Fragment>
-              );
-            }}
-          />
-        </React.Fragment>
-      );
-      break;
-    case "error":
-      returnValue = <ErrorMessage>{`${showStuff.message}`}</ErrorMessage>;
-      break;
-    default:
-      returnValue = null;
-  }
-
-  return returnValue;
+  return (
+    <React.Fragment>
+      <FieldLabel labelFor="instanceName" label="Instance" />
+      <div>
+        <WrappedField
+          id="instanceName"
+          name="instanceName"
+          component={Select}
+          componentClassName="u-fieldLong"
+          options={getInstanceOptions(initInfo)}
+        />
+      </div>
+      <FieldArray
+        name="customerIds"
+        render={arrayHelpers => {
+          return (
+            <React.Fragment>
+              <div className="u-gapTop u-alignRight">
+                <Button
+                  id="addCustomerId"
+                  label="Add Customer ID"
+                  onClick={() => {
+                    arrayHelpers.push(getDefaultCustomerId());
+                  }}
+                />
+              </div>
+              {((Array.isArray(values.customerIds) &&
+                values.customerIds.length) ||
+                null) && <Heading variant="subtitle2">Customer IDs</Heading>}
+              <div>
+                {((Array.isArray(values.customerIds) &&
+                  values.customerIds.length) ||
+                  null) &&
+                  values.customerIds.map((customerId, index) => (
+                    <Well key={index}>
+                      <div>
+                        <FieldLabel
+                          labelFor={`namespaceField${index}`}
+                          label="Namespace"
+                        />
+                        <div>
+                          <WrappedField
+                            id={`namespaceField${index}`}
+                            name={`customerIds.${index}.namespace`}
+                            // component={Select}
+                            component={Textfield}
+                            componentClassName="u-fieldLong"
+                            // options={namespaceOptions}
+                          />
+                        </div>
+                      </div>
+                      <div className="u-gapTop">
+                        <FieldLabel labelFor={`idField${index}`} label="ID" />
+                        <div>
+                          <WrappedField
+                            id={`idField${index}`}
+                            name={`customerIds.${index}.id`}
+                            component={Textfield}
+                            componentClassName="u-fieldLong"
+                            supportDataElement
+                          />
+                        </div>
+                      </div>
+                      <div className="u-gapTop">
+                        <InfoTipLayout tip="Uses the SHA-256 hashing algorithm that allows you to pass in customer IDs or email addresses, and pass out hashed IDs. This is an optional Javascript method for sending hashed identifiers. You can continue to use your own methods of hashing prior to sending customer IDs.">
+                          <WrappedField
+                            name={`customerIds.${index}.hashEnabled`}
+                            component={Checkbox}
+                            label="Convert ID to sha256 hash"
+                          />
+                        </InfoTipLayout>
+                      </div>
+                      <div className="u-gapTop">
+                        <FieldLabel
+                          labelFor={`authenticatedStateField${index}`}
+                          label="Authenticated State"
+                        />
+                        <div>
+                          <WrappedField
+                            id={`authenticatedStateField${index}`}
+                            name={`customerIds.${index}.authenticatedState`}
+                            component={Select}
+                            componentClassName="u-fieldLong"
+                            options={authenticatedStateOptions}
+                          />
+                        </div>
+                      </div>
+                      <div className="u-gapTop">
+                        <InfoTipLayout tip="Adobe Experience Platform will use the customer ID as an identifier to help stitch together more information about that individual. If left unchecked, the identifier within this namespace will still be collected but the ECID will be used as the primary identifier for stitching.">
+                          <WrappedField
+                            name={`customerIds.${index}.primary`}
+                            component={Checkbox}
+                            label="Primary"
+                          />
+                        </InfoTipLayout>
+                      </div>
+                      <div className="u-gapTop">
+                        <ModalTrigger>
+                          <Button
+                            id={`deleteButton${index}`}
+                            label="Delete Customer ID"
+                            icon={<Delete />}
+                            variant="action"
+                            disabled={values.customerIds.length === 1}
+                          />
+                          {values.customerIds.length === 1 ? (
+                            <span className="Note u-gapLeft">
+                              You must have at least one customer ID to use this
+                              action.
+                            </span>
+                          ) : null}
+                          <Dialog
+                            title="Delete Customer ID"
+                            id={`deleteCustomerId${index}`}
+                            onConfirm={() => {
+                              arrayHelpers.remove(index);
+                            }}
+                            confirmLabel="Delete"
+                            cancelLabel="Cancel"
+                          >
+                            Would you like to proceed?
+                          </Dialog>
+                        </ModalTrigger>
+                      </div>
+                    </Well>
+                  ))}
+              </div>
+            </React.Fragment>
+          );
+        }}
+      />
+    </React.Fragment>
+  );
 }
 
 CustomerIdWrapper.propTypes = {
