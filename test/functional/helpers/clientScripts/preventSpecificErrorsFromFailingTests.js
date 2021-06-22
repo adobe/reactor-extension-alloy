9a25c4f4d0ce0564aa666cc7e5ad40da7ef38f1e
Aaron Hardy <aahardy@adobe.com>
2021-06-22 11:57:46 -0600

    Added note about how UserReportableErrors that aren't reported are handled.

--- test/functional/helpers/clientScripts/preventSpecificErrorsFromFailingTests.js
+++ test/functional/helpers/clientScripts/preventSpecificErrorsFromFailingTests.js
@@ -22,8 +22,20 @@ governing permissions and limitations under the License.
  * only want the test to fail if there's something wrong with our code.
  */
 window.addEventListener("error", event => {
-  // UserReportableErrors are for error cases we often test for, so they should not
-  // go unhandled because that would automatically fail the test.
+  // UserReportableErrors are intended to be shown to the user within an error boundary.
+  // Error boundaries, however, do not change the error from being considered unhandled to
+  // handled. Because of this, the unhandled error would typically make TestCafe fail
+  // the test. We often test these error cases though, so the fact that the error went
+  // unhandled really shouldn't fail the test. This code prevents the error from failing
+  // the test. Note that if the error is a UserReportableError but was _not_ displayed inside an
+  // error boundary (for example, the error was thrown within async code and reportAsyncError
+  // was not used), we would really prefer that it fail the test, but this is unfeasible
+  // because the error boundary only exposes the error to our application code _after_ our
+  // event listener here as been called. In other words, there's no robust way to know
+  // whether the error will end up being reported in the error boundary. We're left to
+  // assume it will.
+  // https://github.com/facebook/react/issues/10474
+  // https://github.com/facebook/react/issues/13681
   if (event.error && event.error.name === "UserReportableError") {
     console.error(event.error);
     event.preventDefault();
