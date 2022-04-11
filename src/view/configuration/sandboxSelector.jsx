c039f1831fb2e97ed10a08144d5e7b79cea7e27e
Nina Ciocanu <nciocanu@adobe.com>
2022-04-12 00:23:38 +0300

    adding copyright

--- src/view/configuration/sandboxSelector.jsx
+++ src/view/configuration/sandboxSelector.jsx
@@ -9,6 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
+
 import React, { useEffect } from "react";
 import { useAsyncList } from "@react-stately/data";
 import { Item } from "@adobe/react-spectrum";
