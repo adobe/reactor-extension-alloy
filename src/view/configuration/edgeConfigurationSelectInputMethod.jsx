82177462601bc4fc0c3d5b667d4ba94ab4da92fc
Nina Ciocanu <nciocanu@adobe.com>
2022-09-26 15:10:34 +0300

    more refactoring

--- src/view/configuration/edgeConfigurationSelectInputMethod.jsx
+++ src/view/configuration/edgeConfigurationSelectInputMethod.jsx
@@ -12,17 +12,35 @@ governing permissions and limitations under the License.
 
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "@adobe/react-spectrum";
 import EdgeConfigEnvironment from "./edgeConfigEnvironment";
 import { DEVELOPMENT, PRODUCTION, STAGING } from "./constants/environmentType";
+import Alert from "../components/alert";
 
 const EdgeConfigurationSelectInputMethod = ({ name, initInfo, context }) => {
   const { current } = context;
   const { sandboxes, datastreams } = current;
   if (!sandboxes || !datastreams) {
     return (
-      <>
-        <h2>There was an issue while fetching configurations</h2>
-      </>
+      <Alert
+        variant="informative"
+        title="Error loading configurations for this organisation."
+        width="size-5000"
+        marginTop="size-100"
+      >
+        You do not have enough permissions to fetch the organisation
+        configurations. See{" "}
+        <Link>
+          <a
+            href="https://experienceleague.adobe.com/docs/experience-platform/collection/permissions.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Permission management for data collection in Experience Platform
+          </a>
+        </Link>{" "}
+        for more information.
+      </Alert>
     );
   }
   return (
