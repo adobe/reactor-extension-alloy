877d1c0ddb9638a1f5eb86223ffc2001fc4a3e61
Carter McBride <cmcbride@adobe.com>
2023-05-15 10:34:18 -0600

    Move trimValue() from lib/ to view/

--- test/unit/view/utils/trimValues.spec.js
+++ test/unit/view/utils/trimValues.spec.js
@@ -9,7 +9,8 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-import trimValue from "../../../../src/lib/utils/trimValues";
+
+import trimValue from "../../../../src/view/utils/trimValues";
 
 describe("trimValues", () => {
   [
