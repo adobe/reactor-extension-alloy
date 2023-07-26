ad0058196f5aa71d278d378b7d3ef09ffa1dbbb8
Carter McBride <cmcbride@adobe.com>
2023-07-26 11:26:27 -0600

    Provide more user friendly error message for id sync container id issues

--- src/view/components/overrides/overrides.jsx
+++ src/view/components/overrides/overrides.jsx
@@ -213,6 +213,20 @@ const Overrides = ({
                   }
                   return "The value must contain one or more valid data elements.";
                 }
+                if (idSyncContainers.length === 0) {
+                  try {
+                    const parsedValue = parseInt(value, 10);
+                    if (parsedValue < 0) {
+                      return "The value must be a positive number.";
+                    }
+                    if (value.includes(".")) {
+                      return "The value must whole number.";
+                    }
+                    return undefined;
+                  } catch (e) {
+                    return "The value must positive, whole number.";
+                  }
+                }
                 return validateItemIsInContainersList(value);
               };
 
