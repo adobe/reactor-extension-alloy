69d9773974e50f53932d77f16d34819b5cdc791a
Carter McBride <cmcbride@adobe.com>
2023-07-27 08:34:36 -0600

    Support old override settings in runtime

--- test/unit/lib/utils/createGetConfigOverrides.spec.js
+++ test/unit/lib/utils/createGetConfigOverrides.spec.js
@@ -91,4 +91,46 @@ describe("createGetConfigOverrides", () => {
       });
     });
   });
+
+  it("should extract the old settings if they exist", () => {
+    const getConfigOverrides = createGetConfigOverrides("development");
+    const result = getConfigOverrides({
+      edgeConfigOverrides: {
+        com_adobe_experience_platform: {
+          datasets: {
+            event: {
+              datasetId: "6335faf30f5a161c0b4b1444"
+            }
+          }
+        },
+        com_adobe_analytics: {
+          reportSuites: ["unifiedjsqeonly2"]
+        },
+        com_adobe_identity: {
+          idSyncContainerId: 30793
+        },
+        com_adobe_target: {
+          propertyToken: "a15d008c-5ec0-cabd-7fc7-ab54d56f01e8"
+        }
+      }
+    });
+    expect(result).toEqual({
+      com_adobe_experience_platform: {
+        datasets: {
+          event: {
+            datasetId: "6335faf30f5a161c0b4b1444"
+          }
+        }
+      },
+      com_adobe_analytics: {
+        reportSuites: ["unifiedjsqeonly2"]
+      },
+      com_adobe_identity: {
+        idSyncContainerId: 30793
+      },
+      com_adobe_target: {
+        propertyToken: "a15d008c-5ec0-cabd-7fc7-ab54d56f01e8"
+      }
+    });
+  });
 });
