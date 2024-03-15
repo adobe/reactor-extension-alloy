9860449ecadfbf3c8d1380bad021d35974a56e3b
Nina Ciocanu <nciocanu@adobe.com>
2024-03-15 15:52:40 +0200

    update eventTypes to extract the media events

--- src/view/actions/constants/eventTypes.js
+++ src/view/actions/constants/eventTypes.js
@@ -1,3 +1,15 @@
+/*
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
 export default {
   "advertising.completes": "Advertising Completes",
   "advertising.timePlayed": "Advertising Time Played",
@@ -72,25 +84,6 @@ export default {
   "inappmessageTracking.dismiss": "inapp message was dimissed",
   "inappmessageTracking.display": "inapp message was displayed",
   "inappmessageTracking.interact": "inapp message was interacted with",
-  "media.ping": "Media ping",
-  "media.sessionStart": "Media sessionStart",
-  "media.play": "Media play",
-  "media.bufferStart": "Media bufferStart",
-  "media.pauseStart": "Media pauseStart",
-  "media.chapterStart": "Media chapterStart",
-  "media.chapterSkip": "Media chapterSkip",
-  "media.chapterComplete": "Media chapterComplete",
-  "media.adStart": "Media adStart",
-  "media.adSkip": "Media adSkip",
-  "media.adComplete": "Media adComplete",
-  "media.adBreakStart": "Media adBreakStart",
-  "media.adBreakComplete": "Media adBreakComplete",
-  "media.bitrateChange": "Media bitrateChange",
-  "media.error": "Media error",
-  "media.sessionComplete": "Media sessionComplete",
-  "media.sessionEnd": "Media sessionEnd",
-  "media.statesUpdate": "Media statesUpdate",
-  "media.downloaded": "Media downloaded content",
   "media.reporting.sessionStart": "Media reporting sessionStart",
   "media.reporting.sessionClose": "Media reporting sessionClose",
   "media.reporting.adStart": "Media reporting adStart",
