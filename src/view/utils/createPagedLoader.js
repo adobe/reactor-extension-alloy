f92373bb736e10f4b07cf7b5b6aa78e752a27d7b
Jon Snyder <josnyder@adobe.com>
2024-05-20 15:32:37 -0600

    remove console log

--- src/view/utils/createPagedLoader.js
+++ src/view/utils/createPagedLoader.js
@@ -46,7 +46,6 @@ export default ({
         signal: currentAbortController.signal
       }));
     } catch (e) {
-      console.log(e);
       // Most of the times this is an AbortError, but for everything
       // else the loadItems function should handle the error itself.
       if (e.name !== "AbortError") {
