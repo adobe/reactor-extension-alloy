7da19841c7701473e42138ff73a2aebb4e38d19a
Jon Snyder <josnyder@adobe.com>
2024-07-10 13:47:01 -0600

    Update auth using fetch based call

--- test/functional/helpers/getAdobeIOAccessToken.js
+++ test/functional/helpers/getAdobeIOAccessToken.js
@@ -1,29 +1,74 @@
-/*
-Copyright 2020 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
+const fs = require("fs");
+const fetch = require("node-fetch");
 
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
-import auth from "@adobe/jwt-auth";
-import adobeIOClientCredentials from "./adobeIOClientCredentials";
+module.exports = (credentials) => {
+  const { clientId, clientSecret } = credentials;
+  const tokenPath = ".token-cache.json";
 
-let accessToken;
-
-/**
- * Retrieves an access token for the Adobe I/O integration used for
- * running end-to-end tests.
- * @returns {Promise<string>}
- */
-export default async () => {
-  if (accessToken) {
-    return accessToken;
+  // Check if token is cached not expired
+  if (fs.existsSync(tokenPath)) {
+    const cachedToken = JSON.parse(fs.readFileSync(tokenPath, "utf8"));
+    if (new Date().getTime() < cachedToken.expiry) {
+      return cachedToken.access_token;
+    }
   }
-  const result = await auth(adobeIOClientCredentials);
-  accessToken = result.access_token;
-  return accessToken;
+
+  if (!clientSecret) {
+    throw new Error("Missing client secret.");
+  }
+
+  // Fetch new token
+  const params = {
+    grant_type: "client_credentials",
+    client_id: clientId,
+    client_secret: clientSecret,
+    scope: [
+      "reactor_approver",
+      "reactor_publisher",
+      "assurance_read_plugins",
+      "reactor_developer",
+      "assurance_read_annotations",
+      "openid",
+      "session",
+      "AdobeID",
+      "read_organizations",
+      "reactor_extension_developer",
+      "assurance_read_events",
+      "additional_info.job_function",
+      "assurance_manage_sessions",
+      "reactor_it_admin",
+      "assurance_read_session_annotations",
+      "reactor_admin",
+      "additional_info.roles",
+      "additional_info.projectedProductContext",
+      "assurance_read_clients",
+    ].join(","),
+  };
+
+  const formBody = Object.keys(params)
+    .map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`,
+    )
+    .join("&");
+
+  return fetch("https://ims-na1.adobelogin.com/ims/token/v3", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: formBody,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Calculate expiry time (current time + expires_in seconds - 2 hour buffer)
+      const expiry = new Date().getTime() + (data.expires_in - 7200) * 1000;
+      const accessToken = data.access_token;
+      // Cache the token
+      fs.writeFileSync(
+        tokenPath,
+        JSON.stringify({ access_token: accessToken, expiry }),
+        "utf8",
+      );
+      return accessToken;
+    });
 };
