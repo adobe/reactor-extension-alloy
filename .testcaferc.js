44cacff0008818277cb9b04ae123d002ec421ff3
Jon Snyder <josnyder@adobe.com>
2024-08-15 17:12:49 -0600

    add quotes on browser, add testcafe defaults

---
+++ .testcaferc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  skipJsErrors: true,
+  quarantineMode: true,
+  selectorTimeout: 50000,
+  assertionTimeout: 7000,
+  pageLoadTimeout: 8000,
+  speed: 1.0,
+  stopOnFirstFail: false,
+};
