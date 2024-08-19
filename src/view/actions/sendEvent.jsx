1dea43e2873df7e4d1f26bfefe83a20109e673f8
Jon Snyder <josnyder@adobe.com>
2024-08-19 09:48:32 -0600

    Remove beta labels for guided events and requesting default personalization

--- src/view/actions/sendEvent.jsx
+++ src/view/actions/sendEvent.jsx
@@ -168,7 +168,6 @@ const includeRenderedPropositionsField = checkbox({
   description:
     'Check this to use this event as a display event, including the propositions that rendered when "automatically send a display event" was unchecked. This will populate the `_experience.decisioning` XDM field with information about rendered personalization.',
   defaultValue: false,
-  beta: true,
 });
 
 const disabledIncludeRenderedPropositionsField = disabledCheckbox({
@@ -177,7 +176,6 @@ const disabledIncludeRenderedPropositionsField = disabledCheckbox({
   description:
     'Check this to use this event as a display event, including the propositions that rendered when "automatically send a display event" was unchecked. This will populate the `_experience.decisioning` XDM field with information about rendered personalization.',
   value: true,
-  beta: true,
 });
 
 const documentUnloadingField = checkbox({
@@ -242,7 +240,6 @@ const sendDisplayEventField = conditional(
       description:
         'Check this to send a display event after personalization is rendered. Uncheck this to include the display notifications in a subsequent event with the "include rendered propositions" checked.',
       defaultValue: true,
-      beta: true,
     }),
   ],
 );
@@ -253,7 +250,6 @@ const sendDisplayEventUnchecked = disabledCheckbox({
   description:
     'Check this to send a display event after personalization is rendered. Uncheck this to include the display notifications in a subsequent event with the "include rendered propositions" checked.',
   value: false,
-  beta: true,
 });
 
 const defaultPersonalizationEnabledField = radioGroup({
@@ -278,7 +274,6 @@ const defaultPersonalizationEnabledField = radioGroup({
         "Disabled - explicitly suppress the request for the page scope and default surface.",
     },
   ],
-  beta: true,
 });
 
 const decisionContext = simpleMap({
@@ -318,7 +313,6 @@ const sendEventForm = form(
       description:
         "Check this box to automatically fill in or hide certain fields to enable a particular use-case.",
       defaultValue: false,
-      beta: true,
     }),
     conditional(
       {
@@ -369,7 +363,6 @@ const sendEventForm = form(
                 "Collect analytics - record an event without getting personalization decisions. This is meant to be called late in the page load.",
             },
           ],
-          beta: true,
         }),
         conditional(
           {
