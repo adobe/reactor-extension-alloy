89e6ace06182a16c5385235defa4c364ab1649ca
Shamiul Mowla <sham.mowla@gmail.com>
2024-08-27 16:58:21 -0400

    update to the latest browsers.

--- scripts/functional.mjs
+++ scripts/functional.mjs
@@ -88,16 +88,13 @@ const buildComponentFixtures = async () => {
   let browsers;
 
   if (chrome) {
-    browsers = "saucelabs:chrome@123:Mac 13";
+    browsers = "saucelabs:chrome@latest:Mac 13"; 
     concurrency = 4;
   } else if (firefox) {
-    browsers = "saucelabs:firefox@123:Mac 13";
+    browsers = "saucelabs:firefox@latest:Mac 13"; 
     concurrency = 4;
   } else if (safari) {
-    browsers = "saucelabs:safari@17:Mac 13";
-    concurrency = 4;
-  } else if (edge) {
-    browsers = "saucelabs:MicrosoftEdge@121:Windows 11";
+    browsers = "saucelabs:safari@latest:Mac 13"; 
     concurrency = 4;
   } else {
     concurrency = 1;
