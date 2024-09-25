898da23119f05895ffdb62dbb0fec51f99202550
Jon Snyder <josnyder@adobe.com>
2024-09-25 09:57:53 -0600

    Update version of reactor-releaser to fix error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,5 +23,5 @@ npx @adobe/reactor-uploader@6.0.0-beta.12 package-adobe-alloy-${VERSION}.zip \
   --auth.client-id=f401a5fe22184c91a85fd441a8aa2976 \
   --upload-timeout=300
 
-echo "Y" | npx @adobe/reactor-releaser@4.0.0-beta.3 \
+echo "Y" | npx @adobe/reactor-releaser@4.0.0-beta.8 \
   --auth.client-id=f401a5fe22184c91a85fd441a8aa2976
