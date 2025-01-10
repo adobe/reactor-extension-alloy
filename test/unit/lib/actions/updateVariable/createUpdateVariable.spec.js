9feee36fb6a5bda3bcda81314ed4758e0fa9fa8f
Shamiul Mowla <sham.mowla@gmail.com>
2025-01-10 15:31:07 -0500

    Remove global test functions in favor of direct imports. (#451)

--- test/unit/lib/actions/updateVariable/createUpdateVariable.spec.js
+++ test/unit/lib/actions/updateVariable/createUpdateVariable.spec.js
@@ -10,6 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+import { describe, it, expect, beforeEach } from "vitest";
 import deepAssign from "../../../helpers/deepAssign";
 import createUpdateVariable from "../../../../../src/lib/actions/updateVariable/createUpdateVariable";
 
--- test/unit/view/utils/deepGet.spec.js
+++ test/unit/view/utils/deepGet.spec.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import deepGet from "../../../../src/view/utils/deepGet";
 
 describe("deepGet", () => {
