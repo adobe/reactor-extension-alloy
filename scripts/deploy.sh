9f8613ac8ef40942abbabf6c4d1258a406b7b7b6
Carter McBride <cmcbride@adobe.com>
2025-09-18 11:21:12 -0600

    Migrate workflows to pnpm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,9 +19,9 @@ git push gh-origin HEAD:main --follow-tags
 
 pnpm run package
 
-npx @adobe/reactor-uploader@6.0.0-beta.12 package-adobe-alloy-${VERSION}.zip \
+pnpx @adobe/reactor-uploader@6.0.0-beta.12 package-adobe-alloy-${VERSION}.zip \
   --auth.client-id=f401a5fe22184c91a85fd441a8aa2976 \
   --upload-timeout=300
 
-echo "Y" | npx @adobe/reactor-releaser@4.0.0-beta.8 \
+echo "Y" | pnpx @adobe/reactor-releaser@4.0.0-beta.8 \
   --auth.client-id=f401a5fe22184c91a85fd441a8aa2976
